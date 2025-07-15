1caeaa6def63c9b5c303a8ba9dc0b925bedb91da
dktpt44 <dhurba6don@gmail.com>
2025-07-15 23:43:33 +0400

    updated the news page

--- js/chatscript.js
+++ js/chatscript.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-link");
-
   navLinks.forEach(link => {
     link.addEventListener("click", function () {
       // Remove 'active' from all links
@@ -8,6 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // Add 'active' to the clicked link
       this.classList.add("active");
+    });
+  });
+
+  document.querySelectorAll('a.nav-link[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const sectionId = link.getAttribute("href");
+      window.location.href = "index.html" + sectionId;
     });
   });
 });
