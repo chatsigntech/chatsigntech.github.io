a1fbcd3d1387aa25e77f675c3eed2c6b939d3f30
lizh <lizh@company.com>
2026-08-29 11:24:40 +0400

    Deploy updated ChatSign website from 51ceaaa

---
+++ web3forms-config.js
@@ -0,0 +1,5 @@
+window.CHATSIGN_CONFIG = Object.assign({}, window.CHATSIGN_CONFIG, {
+  // The GitHub Pages maintainer may place the Web3Forms access key here, or
+  // provide VITE_WEB3FORMS_ACCESS_KEY while building the source project.
+  web3formsAccessKey: '',
+})
